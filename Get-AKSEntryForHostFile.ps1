50736123100ed85aeb8d15c2a2513adc61dde415
Vishal Anand <vishal.anand225@gmail.com>
2025-07-23 15:39:25 +0530

    Create Get-AKSEntryForHostFile.ps1

---
+++ Get-AKSEntryForHostFile.ps1
@@ -0,0 +1,48 @@
+$host=@()
+function Get-PvtIpAddress {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$uri,
+
+        [Parameter(Mandatory = $true)]
+        [string]$token
+    )
+
+    $headers = @{
+        "Content-Type" = "application/json"
+        "Authorization" = "Bearer $token"
+    }
+
+    $detail = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
+
+    return $detail.properties.ipConfigurations.properties.privateIPAddress
+}
+Login-azaccount 
+$subscriptionsid = (Get-AzSubscription).Id
+foreach ($subscriptionId in $subscriptionsId) {
+    set-azcontext $subscriptionId
+    $aksclusters = Get-AzAksCluster
+    if($aksclusters){
+        $token = (Get-AzAccessToken).Token
+    }           
+    
+    foreach ($akscluster in $aksclusters) {
+        $ManagedRG = $akscluster.NodeResourceGroup
+        $PrivateFQDN = $akscluster.PrivateFQDN
+        $pvepdetail = Get-AzPrivateendpoint -ResourceGroupName $ManagedRG
+        $networkinterfaces = $pvepdetail.NetworkInterfaces.Id
+        foreach ($networkinterface in $networkinterfaces) {
+            $uri = "https://management.azure.com$($networkinterface)?api-version=2024-01-01"
+            $ipaddress = Get-PvtIpAddress -Uri $uri -token $token
+            if($ipaddress){
+                $host1 +=[PSCustomObject]@{
+                    IPAddress = $ipaddress
+                    HostName = $PrivateFQDN
+                    
+                }
+            }
+            
+        }
+    }
+}
+
